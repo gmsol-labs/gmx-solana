81ee6b1d9dc1770532a28bf3bb82c0e29b390863
orion-047 <161582484+orion-047@users.noreply.github.com>
2025-06-05 07:05:55 +0000

    feat(cli): add `admin members` subcommand

--- crates/cli/src/commands/admin.rs
+++ crates/cli/src/commands/admin.rs
@@ -1,4 +1,8 @@
+use std::collections::BTreeMap;
+
 use gmsol_sdk::ops::StoreOps;
+
+use crate::config::DisplayOptions;
 
 /// Administrative commands.
 #[derive(Debug, clap::Args)]
@@ -9,6 +13,8 @@ pub struct Admin {
 
 #[derive(Debug, clap::Subcommand)]
 enum Command {
+    /// Display member table.
+    Members,
     /// Initialize callback authority.
     InitCallbackAuthority,
 }
@@ -20,9 +26,65 @@ impl super::Command for Admin {
 
     async fn execute(&self, ctx: super::Context<'_>) -> eyre::Result<()> {
         let client = ctx.client()?;
+        let store = ctx.store();
         let options = ctx.bundle_options();
+        let output = ctx.config().output();
 
         let bundle = match &self.command {
+            Command::Members => {
+                const ADMIN: &str = "ADMIN";
+                let store = client.store(store).await?;
+                let role_store = &store.role;
+                let roles = std::iter::once(Ok(ADMIN))
+                    .chain(role_store.roles().map(|res| res.map_err(eyre::Error::from)))
+                    .collect::<eyre::Result<Vec<_>>>()?;
+                let mut members = role_store
+                    .members()
+                    .map(|member| {
+                        let roles = roles
+                            .iter()
+                            .filter_map(|role| {
+                                if *role == ADMIN {
+                                    if store.authority == member {
+                                        Some(Ok(ADMIN))
+                                    } else {
+                                        None
+                                    }
+                                } else {
+                                    match role_store.has_role(&member, role) {
+                                        Ok(true) => Some(Ok(*role)),
+                                        Ok(false) => None,
+                                        Err(err) => Some(Err(err)),
+                                    }
+                                }
+                            })
+                            .collect::<Result<Vec<_>, _>>()?
+                            .join("|");
+                        Ok((
+                            member,
+                            serde_json::json!({
+                                "roles": roles,
+                            }),
+                        ))
+                    })
+                    .collect::<eyre::Result<BTreeMap<_, _>>>()?;
+                members.entry(store.authority).or_insert_with(|| {
+                    serde_json::json!({
+                    "roles": ADMIN,
+                    })
+                });
+                println!(
+                    "{}",
+                    output.display_keyed_accounts(
+                        members,
+                        DisplayOptions::table_projection([
+                            ("pubkey", "Member"),
+                            ("roles", "Roles")
+                        ])
+                    )?
+                );
+                return Ok(());
+            }
             Command::InitCallbackAuthority => client
                 .initialize_callback_authority()
                 .into_bundle_with_options(options)?,
