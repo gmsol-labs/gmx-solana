6191c33b12ba127b06236692795eb9709b26bcfb
orion-047 <161582484+orion-047@users.noreply.github.com>
2025-06-05 08:05:30 +0000

    feat(cli): complete the implementation of `admin` subcommand

--- crates/cli/src/commands/admin.rs
+++ crates/cli/src/commands/admin.rs
@@ -1,8 +1,20 @@
 use std::collections::BTreeMap;
 
-use gmsol_sdk::ops::StoreOps;
+use gmsol_sdk::{
+    ops::{RoleOps, StoreOps},
+    programs::anchor_lang::prelude::Pubkey,
+    solana_utils::{
+        bundle_builder::{BundleBuilder, BundleOptions},
+        signer::LocalSignerRef,
+        solana_sdk::signature::NullSigner,
+        transaction_builder::default_before_sign,
+    },
+};
+use indexmap::IndexSet;
 
 use crate::config::DisplayOptions;
+
+use super::CommandClient;
 
 /// Administrative commands.
 #[derive(Debug, clap::Args)]
@@ -17,6 +29,45 @@ enum Command {
     Members,
     /// Display role table.
     Roles,
+    /// Initialize a new store.
+    InitStore { key: String },
+    /// Transfer store authority.
+    TransferStoreAuthority {
+        #[arg(long)]
+        new_authority: Pubkey,
+        #[arg(long)]
+        confirm: bool,
+    },
+    /// Accept store authority.
+    AcceptStoreAuthority,
+    /// Transfer receiver.
+    TransferReceiver {
+        new_receiver: Pubkey,
+        #[arg(long)]
+        confirm: bool,
+    },
+    /// Enable a role.
+    EnableRole { role: String },
+    /// Disable a role.
+    DisableRole { role: String },
+    /// Grant a role to a user.
+    GrantRole {
+        /// User.
+        authority: Pubkey,
+        /// Role.
+        #[arg(long)]
+        role: String,
+    },
+    /// Revoke a role from the user.
+    RevokeRole {
+        /// User.
+        authority: Pubkey,
+        /// Role.
+        #[arg(long)]
+        role: String,
+    },
+    /// Initialize roles.
+    InitRoles(Box<InitializeRoles>),
     /// Initialize callback authority.
     InitCallbackAuthority,
 }
@@ -35,32 +86,53 @@ impl super::Command for Admin {
         let bundle = match &self.command {
             Command::Members => {
                 const ADMIN: &str = "ADMIN";
+                const RECEIVER: &str = "RECEIVER";
+                const HOLDING: &str = "HOLDING";
                 let store = client.store(store).await?;
                 let role_store = &store.role;
                 let roles = std::iter::once(Ok(ADMIN))
+                    .chain(Some(Ok(RECEIVER)))
+                    .chain(Some(Ok(HOLDING)))
                     .chain(role_store.roles().map(|res| res.map_err(eyre::Error::from)))
                     .collect::<eyre::Result<Vec<_>>>()?;
-                let mut members = role_store
+                let members = role_store
                     .members()
+                    .chain(Some(store.authority))
+                    .chain(Some(store.treasury.receiver))
+                    .chain(Some(store.address.holding))
+                    .collect::<IndexSet<_>>();
+                let members = members
+                    .into_iter()
                     .map(|member| {
                         let roles = roles
                             .iter()
                             .filter_map(|role| {
                                 if *role == ADMIN {
                                     if store.authority == member {
-                                        Some(Ok(ADMIN))
+                                        Some(ADMIN)
+                                    } else {
+                                        None
+                                    }
+                                } else if *role == RECEIVER {
+                                    if store.treasury.receiver == member {
+                                        Some(RECEIVER)
+                                    } else {
+                                        None
+                                    }
+                                } else if *role == HOLDING {
+                                    if store.address.holding == member {
+                                        Some(HOLDING)
                                     } else {
                                         None
                                     }
                                 } else {
                                     match role_store.has_role(&member, role) {
-                                        Ok(true) => Some(Ok(*role)),
-                                        Ok(false) => None,
-                                        Err(err) => Some(Err(err)),
+                                        Ok(true) => Some(*role),
+                                        _ => None,
                                     }
                                 }
                             })
-                            .collect::<Result<Vec<_>, _>>()?
+                            .collect::<Vec<_>>()
                             .join("|");
                         Ok((
                             member,
@@ -70,11 +142,7 @@ impl super::Command for Admin {
                         ))
                     })
                     .collect::<eyre::Result<BTreeMap<_, _>>>()?;
-                members.entry(store.authority).or_insert_with(|| {
-                    serde_json::json!({
-                    "roles": ADMIN,
-                    })
-                });
+
                 println!(
                     "{}",
                     output.display_keyed_accounts(
@@ -112,6 +180,80 @@ impl super::Command for Admin {
                 );
                 return Ok(());
             }
+            Command::InitStore { key } => {
+                let store_key = key;
+                let store = client.find_store_address(store_key);
+                let authority = client.payer();
+                tracing::info!(
+                    "Initialize store with key={store_key}, address={store}, admin={}",
+                    authority
+                );
+                client
+                    .initialize_store::<NullSigner>(store_key, None, None, None)
+                    .into_bundle_with_options(options)?
+            }
+            Command::TransferStoreAuthority {
+                new_authority,
+                confirm,
+            } => {
+                let rpc = client.transfer_store_authority(store, new_authority);
+                if *confirm || client.serialize_only.is_some() {
+                    rpc.into_bundle_with_options(options)?
+                } else {
+                    let transaction = rpc
+                        .signed_transaction_with_options(true, None, default_before_sign)
+                        .await?;
+                    let response = client
+                        .store_program()
+                        .rpc()
+                        .simulate_transaction(&transaction)
+                        .await?;
+                    println!("Simulation result: {:#?}", response.value);
+                    if response.value.err.is_none() {
+                        println!("The simulation was successful, but this operation is very dangerous. If you are sure you want to proceed, please reauthorize the command with `--confirm` flag");
+                    }
+                    return Ok(());
+                }
+            }
+            Command::AcceptStoreAuthority => client
+                .accept_store_authority(store)
+                .into_bundle_with_options(options)?,
+            Command::TransferReceiver {
+                new_receiver,
+                confirm,
+            } => {
+                let rpc = client.transfer_receiver(store, new_receiver);
+                if *confirm || client.serialize_only.is_some() {
+                    rpc.into_bundle_with_options(options)?
+                } else {
+                    let transaction = rpc
+                        .signed_transaction_with_options(true, None, default_before_sign)
+                        .await?;
+                    let response = client
+                        .store_program()
+                        .rpc()
+                        .simulate_transaction(&transaction)
+                        .await?;
+                    println!("Simulation result: {:#?}", response.value);
+                    if response.value.err.is_none() {
+                        println!("The simulation was successful, but this operation is very dangerous. If you are sure you want to proceed, please reauthorize the command with `--confirm` flag");
+                    }
+                    return Ok(());
+                }
+            }
+            Command::EnableRole { role } => client
+                .enable_role(store, role)
+                .into_bundle_with_options(options)?,
+            Command::DisableRole { role } => client
+                .disable_role(store, role)
+                .into_bundle_with_options(options)?,
+            Command::GrantRole { authority, role } => client
+                .grant_role(store, authority, role)
+                .into_bundle_with_options(options)?,
+            Command::RevokeRole { authority, role } => client
+                .revoke_role(store, authority, role)
+                .into_bundle_with_options(options)?,
+            Command::InitRoles(args) => args.to_bundle(client, options)?,
             Command::InitCallbackAuthority => client
                 .initialize_callback_authority()
                 .into_bundle_with_options(options)?,
@@ -119,5 +261,93 @@ impl super::Command for Admin {
 
         client.send_or_serialize(bundle).await?;
         Ok(())
+    }
+}
+
+#[derive(Debug, clap::Args)]
+struct InitializeRoles {
+    #[arg(long)]
+    init_store: Option<String>,
+    #[arg(long)]
+    treasury_admin: Pubkey,
+    #[arg(long)]
+    treasury_withdrawer: Pubkey,
+    #[arg(long)]
+    treasury_keeper: Pubkey,
+    #[arg(long)]
+    timelock_admin: Pubkey,
+    #[arg(long)]
+    market_keeper: Pubkey,
+    #[arg(long)]
+    order_keeper: Vec<Pubkey>,
+}
+
+impl InitializeRoles {
+    fn to_bundle<'a>(
+        &self,
+        client: &'a CommandClient,
+        options: BundleOptions,
+    ) -> eyre::Result<BundleBuilder<'a, LocalSignerRef>> {
+        use gmsol_sdk::{core::role::RoleKey, programs::constants::roles};
+
+        let mut builder = client.bundle_with_options(options);
+
+        let store = if let Some(key) = self.init_store.as_ref() {
+            builder.push(client.initialize_store::<NullSigner>(key, None, None, None))?;
+            client.find_store_address(key)
+        } else {
+            client.store
+        };
+
+        let treasury_global_config = client.find_treasury_config_address(&store);
+
+        builder
+            .push_many(
+                [
+                    RoleKey::RESTART_ADMIN,
+                    RoleKey::GT_CONTROLLER,
+                    RoleKey::MARKET_KEEPER,
+                    RoleKey::ORDER_KEEPER,
+                    RoleKey::PRICE_KEEPER,
+                    RoleKey::FEATURE_KEEPER,
+                    RoleKey::CONFIG_KEEPER,
+                    RoleKey::ORACLE_CONTROLLER,
+                    roles::TREASURY_OWNER,
+                    roles::TREASURY_ADMIN,
+                    roles::TREASURY_WITHDRAWER,
+                    roles::TREASURY_KEEPER,
+                    roles::TIMELOCK_ADMIN,
+                    roles::TIMELOCK_KEEPER,
+                    roles::TIMELOCKED_ADMIN,
+                ]
+                .iter()
+                .map(|role| client.enable_role(&store, role)),
+                false,
+            )?
+            .push(client.grant_role(&store, &self.market_keeper, RoleKey::MARKET_KEEPER))?
+            .push(client.grant_role(&store, &treasury_global_config, RoleKey::ORACLE_CONTROLLER))?
+            .push(client.grant_role(&store, &treasury_global_config, RoleKey::GT_CONTROLLER))?
+            .push(client.grant_role(&store, &self.treasury_admin, roles::TREASURY_ADMIN))?
+            .push(client.grant_role(
+                &store,
+                &self.treasury_withdrawer,
+                roles::TREASURY_WITHDRAWER,
+            ))?
+            .push(client.grant_role(&store, &self.treasury_keeper, roles::TREASURY_KEEPER))?
+            .push(client.grant_role(&store, &self.timelock_admin, roles::TIMELOCK_ADMIN))?
+            .push(client.grant_role(&store, &self.timelock_admin, roles::TIMELOCK_KEEPER))?
+            .push(client.grant_role(&store, &self.timelock_admin, roles::TIMELOCKED_ADMIN))?;
+
+        for keeper in self.unique_order_keepers() {
+            builder
+                .push(client.grant_role(&store, keeper, RoleKey::ORDER_KEEPER))?
+                .push(client.grant_role(&store, keeper, RoleKey::PRICE_KEEPER))?;
+        }
+
+        Ok(builder)
+    }
+
+    fn unique_order_keepers(&self) -> impl IntoIterator<Item = &Pubkey> {
+        self.order_keeper.iter().collect::<IndexSet<_>>()
     }
 }
