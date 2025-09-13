1a1ff340108f8a7f28383c725b5beb0423a04f32
orion-047 <161582484+orion-047@users.noreply.github.com>
2025-09-13 07:51:47 +0000

    test: add more tests for MARKET_CONFIG_KEEPER

--- tests/anchor_test/market.rs
+++ tests/anchor_test/market.rs
@@ -2,6 +2,7 @@ use gmsol_programs::anchor_lang;
 use gmsol_sdk::{client::ops::MarketOps, constants::MARKET_USD_UNIT, ops::ExchangeOps};
 use gmsol_store::CoreError;
 use gmsol_utils::market::{MarketConfigFlag, MarketConfigKey};
+use solana_sdk::{signature::Keypair, signer::Signer};
 use tracing::Instrument;
 
 use crate::anchor_test::setup::{current_deployment, Deployment};
@@ -167,8 +168,44 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
 
     let client = deployment.user_client(Deployment::DEFAULT_KEEPER)?;
     let market_config_keeper = deployment.user_client(Deployment::USER_1)?;
+    let user = deployment.user_client(Deployment::DEFAULT_USER)?;
 
-    // 1. Should throw permission denied error.
+    // 0. Prepare buffers.
+    let buffer_keypair = Keypair::new();
+    let buffer = buffer_keypair.pubkey();
+    let signature = market_config_keeper
+        .initialize_market_config_buffer(store, &buffer_keypair, u32::MAX)
+        .send_without_preflight()
+        .await?;
+    tracing::info!("created a new buffer `{buffer}` in {signature}");
+
+    let signature = market_config_keeper
+        .push_to_market_config_buffer(
+            &buffer,
+            [(MarketConfigKey::MinPositionSizeUsd, MARKET_USD_UNIT)],
+        )
+        .send_without_preflight()
+        .await?;
+    tracing::info!("pushed config update to buffer `{buffer}` in {signature}");
+
+    let buffer_keypair = Keypair::new();
+    let buffer_2 = buffer_keypair.pubkey();
+    let signature = user
+        .initialize_market_config_buffer(store, &buffer_keypair, u32::MAX)
+        .send_without_preflight()
+        .await?;
+    tracing::info!("created buffer_2 `{buffer_2}` in {signature}");
+
+    let signature = user
+        .push_to_market_config_buffer(
+            &buffer_2,
+            [(MarketConfigKey::MinPositionSizeUsd, MARKET_USD_UNIT)],
+        )
+        .send_without_preflight()
+        .await?;
+    tracing::info!("pushed config update to buffer_2 `{buffer_2}` in {signature}");
+
+    // 1. Should throw permission denied errors.
     let err = market_config_keeper
         .update_market_config_flag_by_key(
             store,
@@ -195,6 +232,19 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
         .send()
         .await
         .expect_err("this factor should not be updatable by a MARKET_CONFIG_KEEPER");
+
+    assert_eq!(
+        gmsol_sdk::Error::from(err).anchor_error_code(),
+        Some(CoreError::PermissionDenied.into())
+    );
+
+    let err = market_config_keeper
+        .update_market_config_with_buffer(store, market_token, &buffer)
+        .send()
+        .await
+        .expect_err(
+            "this buffer cannot be used to update the market config by a MARKET_CONFIG_KEEPER",
+        );
 
     assert_eq!(
         gmsol_sdk::Error::from(err).anchor_error_code(),
@@ -210,7 +260,7 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
         )?
         .send_without_preflight()
         .await?;
-    tracing::info!("set market config updatable at {signature}");
+    tracing::info!("set market config updatable in {signature}");
 
     // 3. Should update market configs successfully.
     let signature = market_config_keeper
@@ -222,7 +272,7 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
         )?
         .send()
         .await?;
-    tracing::info!("updated flag at {signature}");
+    tracing::info!("updated flag in {signature}");
 
     let signature = market_config_keeper
         .update_market_config_by_key(
@@ -233,9 +283,70 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
         )?
         .send()
         .await?;
-    tracing::info!("updated flag at {signature}");
+    tracing::info!("updated factor in {signature}");
 
-    // 4. Should set market config updatable to false successfully.
+    let signature = market_config_keeper
+        .update_market_config_with_buffer(store, market_token, &buffer)
+        .send()
+        .await?;
+    tracing::info!("updated config with buffer in {signature}");
+
+    // 4. Should throw permission denied errors.
+    let err = user
+        .update_market_config_flag_by_key(
+            store,
+            market_token,
+            MarketConfigFlag::SkipBorrowingFeeForSmallerSide,
+            true,
+        )?
+        .send()
+        .await
+        .expect_err("this flag should not be updatable by an unauthorized address");
+
+    assert_eq!(
+        gmsol_sdk::Error::from(err).anchor_error_code(),
+        Some(CoreError::PermissionDenied.into())
+    );
+
+    let err = user
+        .update_market_config_by_key(
+            store,
+            market_token,
+            MarketConfigKey::MinPositionSizeUsd,
+            &MARKET_USD_UNIT,
+        )?
+        .send()
+        .await
+        .expect_err("this factor should not be updatable by an unauthorized address");
+
+    assert_eq!(
+        gmsol_sdk::Error::from(err).anchor_error_code(),
+        Some(CoreError::PermissionDenied.into())
+    );
+
+    let err = user
+        .update_market_config_with_buffer(store, market_token, &buffer_2)
+        .send()
+        .await
+        .expect_err("this instruction cannot be invoked by an unauthorized address");
+
+    assert_eq!(
+        gmsol_sdk::Error::from(err).anchor_error_code(),
+        Some(CoreError::PermissionDenied.into())
+    );
+
+    let err = client
+        .update_market_config_with_buffer(store, market_token, &buffer)
+        .send()
+        .await
+        .expect_err("the buffer authority must sign");
+
+    assert_eq!(
+        gmsol_sdk::Error::from(err).anchor_error_code(),
+        Some(CoreError::PermissionDenied.into())
+    );
+
+    // 5. Should set market config updatable to false successfully.
     let signature = client
         .set_market_config_updatable(
             store,
@@ -244,9 +355,9 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
         )?
         .send_without_preflight()
         .await?;
-    tracing::info!("set market config updatable at {signature}");
+    tracing::info!("set market config updatable in {signature}");
 
-    // 5. Should throw permission denied error.
+    // 6. Should throw permission denied errors again.
     let err = market_config_keeper
         .update_market_config_flag_by_key(
             store,
@@ -273,6 +384,19 @@ async fn set_market_config_updatable() -> eyre::Result<()> {
         .send()
         .await
         .expect_err("this factor should not be updatable by a MARKET_CONFIG_KEEPER");
+
+    assert_eq!(
+        gmsol_sdk::Error::from(err).anchor_error_code(),
+        Some(CoreError::PermissionDenied.into())
+    );
+
+    let err = market_config_keeper
+        .update_market_config_with_buffer(store, market_token, &buffer)
+        .send()
+        .await
+        .expect_err(
+            "this buffer cannot be used to update the market config by a MARKET_CONFIG_KEEPER",
+        );
 
     assert_eq!(
         gmsol_sdk::Error::from(err).anchor_error_code(),
