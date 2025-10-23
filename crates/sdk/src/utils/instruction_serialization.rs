2b7f8b801f07d8289877804a445839513d36c241
orion-047 <161582484+orion-047@users.noreply.github.com>
2025-10-23 04:47:40 +0000

    feat(sdk): add `base58-legacy` serialization format

--- crates/sdk/src/utils/instruction_serialization.rs
+++ crates/sdk/src/utils/instruction_serialization.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use base64::{engine::general_purpose::STANDARD, Engine};
 use solana_sdk::instruction::Instruction;
 use solana_sdk::pubkey::Pubkey;
@@ -13,6 +15,8 @@ pub enum InstructionSerialization {
     #[default]
     /// Base64.
     Base64,
+    /// Base58 (Legacy).
+    Base58Legacy,
 }
 
 /// Serialize an instruction.
@@ -24,10 +28,14 @@ pub fn serialize_instruction(
     use solana_sdk::message::legacy::Message;
 
     let message = match format {
-        InstructionSerialization::Base58 | InstructionSerialization::Base64 => {
+        InstructionSerialization::Base58
+        | InstructionSerialization::Base64
+        | InstructionSerialization::Base58Legacy => {
             let message = Message::new(&[ix.clone()], payer);
             match format {
-                InstructionSerialization::Base58 => bs58::encode(message.serialize()).into_string(),
+                InstructionSerialization::Base58 | InstructionSerialization::Base58Legacy => {
+                    bs58::encode(message.serialize()).into_string()
+                }
                 InstructionSerialization::Base64 => STANDARD.encode(message.serialize()),
             }
         }
@@ -44,6 +52,32 @@ pub fn serialize_message(
     let message = match format {
         InstructionSerialization::Base58 => bs58::encode(message.serialize()).into_string(),
         InstructionSerialization::Base64 => STANDARD.encode(message.serialize()),
+        InstructionSerialization::Base58Legacy => {
+            let message = to_legacy_message(message)?;
+            bs58::encode(message.serialize()).into_string()
+        }
     };
     Ok(message)
+}
+
+/// Convert to legacy message.
+pub fn to_legacy_message<'a>(
+    message: &'a solana_sdk::message::VersionedMessage,
+) -> crate::Result<Cow<'a, solana_sdk::message::legacy::Message>> {
+    use solana_sdk::message::{legacy::Message, VersionedMessage};
+
+    match message {
+        VersionedMessage::Legacy(message) => Ok(Cow::Borrowed(message)),
+        VersionedMessage::V0(message) => {
+            if !message.address_table_lookups.is_empty() {
+                return Err(crate::Error::custom("a v0 message that includes address table lookups cannot be converted to a legacy message"));
+            }
+            Ok(Cow::Owned(Message {
+                header: message.header,
+                account_keys: message.account_keys.clone(),
+                recent_blockhash: message.recent_blockhash,
+                instructions: message.instructions.clone(),
+            }))
+        }
+    }
 }
