e998660c8d052c8773bb230da032eafb8f8f10fd
orion-047 <161582484+orion-047@users.noreply.github.com>
2025-11-10 13:07:44 +0000

    feat(js): add auto-wrapping for deposits

--- crates/sdk/src/js/instructions/create_deposit.rs
+++ crates/sdk/src/js/instructions/create_deposit.rs
@@ -8,7 +8,7 @@ use wasm_bindgen::prelude::*;
 use crate::{
     builders::{
         deposit::{CreateDeposit, CreateDepositHint},
-        token::PrepareTokenAccounts,
+        token::{PrepareTokenAccounts, WrapNative},
         user::PrepareUser,
         StoreProgram,
     },
@@ -56,6 +56,8 @@ pub struct CreateDepositOptions {
     pub hints: HashMap<StringPubkey, CreateDepositHint>,
     #[serde(default)]
     pub transaction_group: TransactionGroupOptions,
+    #[serde(default)]
+    skip_wrap_native_on_pay: Option<bool>,
 }
 
 #[wasm_bindgen]
@@ -75,17 +77,26 @@ pub fn create_deposits_builder(
     let mut tokens = HashSet::default();
     let mut groups: Vec<AtomicGroup> = Vec::with_capacity(deposits.len());
 
+    let should_wrap_native = !options.skip_wrap_native_on_pay.unwrap_or_default();
+    let mut wrap_native = false;
+
     for params in deposits.into_iter() {
         let market_token = params.market_token;
         let hint = options.hints.get(&market_token).ok_or_else(|| {
             crate::Error::custom(format!("hint for {} is not provided", market_token.0))
         })?;
 
+        let mut wrap_amount = 0;
+
         if let Some(t) = params.long_pay_token.as_ref() {
-            tokens.insert(*t);
+            if should_wrap_native && **t == WrapNative::NATIVE_MINT {
+                wrap_amount += params.long_pay_amount.unwrap_or_default();
+            }
         }
         if let Some(t) = params.short_pay_token.as_ref() {
-            tokens.insert(*t);
+            if should_wrap_native && **t == WrapNative::NATIVE_MINT {
+                wrap_amount += params.short_pay_amount.unwrap_or_default();
+            }
         }
         tokens.insert(market_token);
 
@@ -107,10 +118,27 @@ pub fn create_deposits_builder(
             builder.receiver(r).build()
         } else {
             builder.build()
+        }
+        .into_atomic_group(hint)?;
+
+        let ag = if wrap_amount == 0 {
+            built
+        } else {
+            wrap_native = true;
+            let mut wrap = WrapNative::builder()
+                .owner(options.payer)
+                .lamports(wrap_amount.try_into().map_err(crate::Error::custom)?)
+                .build()
+                .into_atomic_group(&true)?;
+            wrap.merge(built);
+            wrap
         };
 
-        let ag = built.into_atomic_group(hint)?;
         groups.push(ag);
+    }
+
+    if wrap_native {
+        tokens.insert(WrapNative::NATIVE_MINT.into());
     }
 
     Ok(CreateDepositsBuilder {
