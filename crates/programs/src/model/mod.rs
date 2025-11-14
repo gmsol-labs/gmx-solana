69da9b1863cdcc08dfa0cdf2a48d4bbd47729d01
hhhhhpp <174809678+hhhhhpp@users.noreply.github.com>
2025-11-14 14:08:24 +0800

    feat: add VirtualInventoryModel to the model module

--- crates/programs/src/model/mod.rs
+++ crates/programs/src/model/mod.rs
@@ -3,6 +3,8 @@ mod market;
 mod pool;
 mod position;
 mod price;
+mod virtual_inventory;
 
 pub use market::{MarketModel, PositionOptions, SwapPricingKind};
 pub use position::PositionModel;
+pub use virtual_inventory::VirtualInventoryModel;
