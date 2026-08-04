2517177ceb10614bfb0ee7144288512882dbb6d8
angelo-75 <187674410+angelo-75@users.noreply.github.com>
2026-08-04 04:17:18 +0000

    refactor(chainlink-datastreams): remove support for v4 reports

--- crates/chainlink-datastreams/src/report.rs
+++ crates/chainlink-datastreams/src/report.rs
@@ -221,27 +221,6 @@ pub fn decode(data: &[u8]) -> Result<Report, DecodeError> {
                 extended_market_status: None,
             })
         }
-        4 => {
-            let report = ReportDataV4::decode(data)?;
-            let price = bigint_to_signed(report.price)?;
-            let market_status = decode_market_status(report.market_status)?;
-            Ok(Report {
-                feed_id: report.feed_id,
-                valid_from_timestamp: report.valid_from_timestamp,
-                observations_timestamp: report.observations_timestamp,
-                last_update_timestamp: None,
-                native_fee: bigint_to_u192(report.native_fee)?,
-                link_fee: bigint_to_u192(report.link_fee)?,
-                expires_at: report.expires_at,
-                price,
-                // Bid and ask values are not available for the first iteration
-                // of the RWA report schema (v4).
-                bid: price,
-                ask: price,
-                market_status,
-                extended_market_status: Some(market_status.into()),
-            })
-        }
         7 => {
             let report = ReportDataV7::decode(data)?;
             let price = bigint_to_signed(report.exchange_rate)?;
